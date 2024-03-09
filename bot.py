9b72379dcf6a22fad636ec4a471f26d031bc4f1c
beneqo0 <158744573+beneqo0@users.noreply.github.com>
2024-03-09 10:45:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import os
+import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,5 +37,34 @@ async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
-    
+@bot.command()
+async def mem_p(ctx):
+    pictures_names = os.listdir('images')
+    meme= random.choice(pictures_names)
+    with open(f'images/{meme}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem_a(ctx):
+    meme_names = os.listdir('animals')
+    memes= random.choice(meme_names)
+    with open(f'images/{memes}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Po wywołaniu polecenia duck program wywołuje funkcję get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+
 bot.run("token")
